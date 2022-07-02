abb74b0207bc3a8de12312b3fba956bebf46106a
Eduardo Hernandez <edhernandez@santandertecnologia.com.ar>
2022-07-02 16:55:35 -0300

    initial commit

---
+++ capitole-exam-infrastructure/src/main/java/com/edh/capitole/CapitoleExamApplication.java
@@ -0,0 +1,13 @@
+package com.edh.capitole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitoleExamApplication.class, args);
+    }
+
+}
---
+++ capitole-exam-infrastructure/src/test/java/com/example/capitole/infrastructure/CapitoleExamInfrastructureApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.capitole.infrastructure;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+abstract class CapitoleExamInfrastructureApplicationTests {
+
+}
